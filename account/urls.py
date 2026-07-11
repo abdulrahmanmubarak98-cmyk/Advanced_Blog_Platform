12ad9735f1f9e806c9e2e492f2a5b89e5831401f
Mubarak Adogu <Abdulrahmanmubarak98@gmail.com>
2026-07-10 22:20:10 -0700

    Add create read features

--- account/urls.py
+++ account/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("register/", views.register, name="register"),
     path("login/", views.login_view, name="login"),
+    path("posts/create/", views.create_post, name="create_post"),
+    path("", views.home, name="home"),
 ]
